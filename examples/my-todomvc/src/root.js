237328982d948233dacf5f0d86bcda64e3465cf1
champagne-randy <randy.champagne@socialtables.com>
2018-03-17 14:02:28 -0400

    rendered some todos

--- examples/my-todomvc/src/root.js
+++ examples/my-todomvc/src/root.js
@@ -13,5 +13,13 @@ import AppContainer from './containers/AppContainer';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//ReactDOM.render(<div>Hello World!</div>, document.getElementById('root'));
 ReactDOM.render(<AppContainer />, document.getElementById('todoapp'));
+
+
+// We will remove these lines later:
+
+import TodoActions from './data/TodoActions';
+
+TodoActions.addTodo('My first task');
+TodoActions.addTodo('Another task');
+TodoActions.addTodo('Finish this tutorial');
